4f9b0178b54e88f4d32be8d7b41e74f6b474f07b
Henoker <hennybany@gmail.com>
2024-03-30 21:00:32 +0300

    feat: add forms.py comment form

--- blango_project/settings.py
+++ blango_project/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # local app
     "blog",
+    # Third_party app
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- blog/views.py
+++ blog/views.py
@@ -17,14 +17,26 @@ def post_detail(request, slug):
     if request.user.is_active:
         if request.method == "POST":
             comment_form = CommentForm(request.POST)
+<<<<<<< HEAD
+=======
+            
+>>>>>>> c2ac073 (feat: add forms.py comment form)
             if comment_form.is_valid():
                 comment = comment_form.save(commit=False)
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
                 return redirect(request.path_info)
+<<<<<<< HEAD
             else:
                 comment_form = CommentForm()
         else:
             comment_form = None
     return render(request, "blog/post-detail.html", {"post":post, comment_form:comment_form})
+=======
+        else:
+            comment_form = CommentForm()
+    else:
+        comment_form = None
+    return render(request, "blog/post-detail.html", {"post":post, comment_form: comment_form})
+>>>>>>> c2ac073 (feat: add forms.py comment form)

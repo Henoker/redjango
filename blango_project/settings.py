27165717056a9edfbc6be91c9e7b993734dcbe77
Henoker <hennybany@gmail.com>
2024-04-06 16:15:52 +0300

    feat: django password argon module

--- blango_project/settings.py
+++ blango_project/settings.py
@@ -79,7 +79,14 @@ class Dev(Configuration):
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-
+    # Password Hashing 
+    # Argon2 Password Hashing Algorithm
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
 
     # Password validation
     # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
